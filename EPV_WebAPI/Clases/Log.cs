7bca5de54e191c31c80f89d73d2a6d32e86824f1
tony tolver <andrademaresantonio00@gmail.com>
2023-05-06 20:15:19 -0600

    Creación de controlador para funcionalidad de login

--- EPV_WebAPI/Clases/Log.cs
+++ EPV_WebAPI/Clases/Log.cs
@@ -2,5 +2,20 @@
 {
     public class Log
     {
+        public static void LogWrite(string texto)
+        {
+            try
+            {
+                StreamWriter sw;
+                string logPath = $"Logs/{DateTime.Now.ToString("yyyy-MM-dd")}-log.txt";
+                sw = (File.Exists(logPath)) ? File.AppendText(logPath) : File.CreateText(logPath);
+                sw.WriteLine($"[{DateTime.Now.ToString("HH:mm:ss")}] {texto}");
+                sw.Close();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
     }
 }
---
+++ EPV_WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EPV_WebAPI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // generamos el token que usara el cliente
+        [HttpPost]
+        [Route("getTokenLogin")]
+        public ActionResult GetTokenLogin(string email, string password)
+        {
+            Clases.Log.LogWrite($"GetTokenLogin: user={email}, password={password}");
+            Clases.Login log = new Clases.Login();
+            return Ok(log.getTokenLogin(email, password));
+        }
+
+        [HttpPost("loginByToken")]
+        public ActionResult LoginByToken(string loginToken) 
+        {
+            Clases.Log.LogWrite($"LoginByToken: logintoken={loginToken}");
+            Clases.Login log = new Clases.Login();
+            string token = log.LoginByToken(loginToken);
+
+            switch (token) 
+            {
+                case "-1": return BadRequest("Limite de tiempo excedido");
+                case "-2": return BadRequest("Usuario o clave incorrectos");
+                case "-3": return BadRequest("No se pudo hacer el login, revise los datos enviados");
+                default: return Ok(token); // regresa token si el login fue correcto 
+            }
+        }
+
+        //cambiar contraseña
+        [HttpPost("setPassword")]
+        public ActionResult SetPassword(string token, string encriptedOldPassword, string encriptedNewPassword)
+        {
+            Clases.Log.LogWrite($"SetPassword: token={token}, encriptedOldPassword={encriptedOldPassword}, encriptedNewPassword={encriptedNewPassword} ");
+            Clases.Login log = new Clases.Login();
+            bool resultado = log.SetPassword(token, encriptedOldPassword, encriptedNewPassword);
+            if (resultado)
+                return Ok(resultado);
+            else
+                return BadRequest(resultado);
+        }
+        //destruir token al cerrar la sesion
+        [HttpPost("logout")]
+        public ActionResult Logout(string token)
+        {
+            Clases.Log.LogWrite("Logout");
+            return Ok("");
+        }
+    }
+}
